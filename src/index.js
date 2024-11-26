9bf269d9711f211227b45a65fa96f84569724a48
Luiz Gomes <louisnuuk@hotmail.com>
2024-11-25 21:59:11 -0300

    feat

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ const yahooFinance = require('yahoo-finance2').default;
 
 const app = express();
 
-app.get('/stock/:symbol', async (req, res) => {
+app.get('/:symbol', async (req, res) => {
     const { symbol } = req.params;
 
-    return res.end(symbol);
+    try {
+        const stockData = await yahooFinance.quote(symbol);
+        const stockRating = stockData.averageAnalystRating
+            ? stockData.averageAnalystRating.split(' - ')[0]
+            : 'Rating não disponível';
+
+        res.json({ symbol, rating: stockRating });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar dados da ação.' });
+    }
 });
 
 app.listen(3000);
