284eb8e76d765cdbe5dd07dbffb00a8e28a74646
Luiz Gomes <louisnuuk@hotmail.com>
2024-11-26 18:14:18 -0300

    feat

--- src/controller/stock/index.ts
+++ src/controller/stock/index.ts
@@ -25,26 +25,41 @@ export default class Stock {
 
         try {
             const stockData: any = await yahooFinance.chart(symbol + '.SA', {
-                period1: new Date('2024-01-01').getTime() / 1000,
+                period1: new Date('2023-01-01').getTime() / 1000,
                 period2: new Date('2024-11-01').getTime() / 1000,
                 interval: '1mo',
             });
 
             let carryOver = 0;
+            let totalStocks = 0;
+            let nextContribution = 1000;
+            const dividendEvents = new Map(
+                stockData.dividends.map((dividend: any) => [dividend.date, dividend.amount])
+            );
 
             return res.json({ 
-                quotes: stockData.quotes.map((quote: any, index: number) => {
+                quotes: stockData.quotes.map((quote: any) => {
                     const averageQuote = (quote.open + quote.close) / 2;
-                    const ordenedStocksWithCarry = (monthyContribution / averageQuote) + carryOver;
-                    const integerPart = Math.floor(ordenedStocksWithCarry);
-                    carryOver = ordenedStocksWithCarry - integerPart;
-                
+                    const rawStocks = (monthyContribution / averageQuote) + carryOver;
+                    const integerPart = Math.floor(rawStocks);
+                    carryOver = rawStocks - integerPart;
                     return {
                         quote: averageQuote,
                         date: formatDate(quote.date),
-                        ordenedStocks: integerPart,
+                        withoutDividendOrdenedStocks: integerPart,
                     };
                 }), 
+                dividends: stockData.events.dividends.map((dividend: any) => ({
+                    amount: dividend.amount,
+                    date: formatDate(dividend.date),
+                })), 
+                totalStocks: stockData.quotes.reduce((total: number, quote: any) => {
+                    const averageQuote = (quote.open + quote.close) / 2;
+                    const rawStocks = (monthyContribution / averageQuote) + carryOver;
+                    const integerPart = Math.floor(rawStocks);
+                    carryOver = rawStocks - integerPart;
+                    return total + integerPart;
+                }, 0)
             });
         } catch (error) {
             res.status(500).json({ error: 'Erro ao buscar dados da ação.' });
