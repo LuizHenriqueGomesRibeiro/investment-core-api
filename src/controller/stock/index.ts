ea057f4c4ce648e5a921adbcb1b1f480bd3a19f5
Luiz Gomes <louisnuuk@hotmail.com>
2024-11-28 11:53:25 -0300

    feat

--- src/controller/stock/index.ts
+++ src/controller/stock/index.ts
@@ -6,6 +6,7 @@ interface GetStockValuesListQuery {
     symbol: string,
     start: string,
     end: string,
+    reinvestDividend: boolean,
 }
 
 export default class Stock {
@@ -25,7 +26,7 @@ export default class Stock {
     }
 
     getStockValuesList = async (req: Request, res: Response) => {
-        const { symbol, start, end } = req.query as unknown as GetStockValuesListQuery;
+        const { symbol, start, end, reinvestDividend } = req.query as unknown as GetStockValuesListQuery;
 
         const monthyContribution = 1000;
 
@@ -36,46 +37,102 @@ export default class Stock {
                 interval: '1mo',
             });
 
-            let carryOver = 0;
-            let cumulativeStocks = 0;
-
             const dividends = stockData.events.dividends.map((dividend: any) => ({
                 amount: dividend.amount,
                 date: formatDate(dividend.date),
             }));
+            
+            let carryOver = 0;
+            let cumulativeStocksWithDividend = 0;
+            let cumulativeStocksWithoutDividend = 0;
+            let adjustedContribution = monthyContribution;
 
             const quotes = stockData.quotes.map((quote: any) => {
                 let date = formatDate(quote.date);
+
                 let dividendPayment = dividends.reduce((sum: any, dividend: any) => {
                     const [dividendDay, dividendMonth, dividendYear] = dividend.date.split('/').map(Number);
                     const [day, month, year] = date.split('/').map(Number);
-                  
+
                     if (dividendMonth === month && dividendYear === year) {
-                        return sum + dividend.amount
+                        return sum + dividend.amount;
                     }
-                  
+
                     return sum;
                 }, 0);
 
                 const averageQuote = (quote.open + quote.close) / 2;
-                const rawStocks = (monthyContribution / averageQuote) + carryOver;
-                const integerPart = Math.floor(rawStocks);
 
-                carryOver = rawStocks - integerPart;
-                cumulativeStocks += integerPart;
+                const rawStocksWithoutDividend = monthyContribution / averageQuote + carryOver;
+                const integerPartWithoutDividend = Math.floor(rawStocksWithoutDividend);
+
+                const rawStocksWithDividend = adjustedContribution / averageQuote + carryOver;
+                const integerPartWithDividend = Math.floor(rawStocksWithDividend);
+
+                carryOver = rawStocksWithDividend - integerPartWithDividend;
+                cumulativeStocksWithoutDividend += integerPartWithoutDividend;
+                cumulativeStocksWithDividend += integerPartWithDividend;
+
+                const contributionWithDividend = adjustedContribution;
+                const contributionWithoutDividend = monthyContribution;
+                adjustedContribution = monthyContribution + dividendPayment * cumulativeStocksWithDividend;
 
                 return {
                     quote: averageQuote,
                     date: date,
-                    withoutDividendOrdenedStocks: integerPart,
-                    totalStocks: cumulativeStocks,
-                    dividendPayment: dividendPayment * cumulativeStocks,
+                    withDividendProperty: cumulativeStocksWithDividend * averageQuote,
+                    withDividendContribution: contributionWithDividend,
+                    withDividendOrdenedStocks: integerPartWithDividend,
+                    withDividendTotalStocks: cumulativeStocksWithDividend,
+                    withDividendPayment: dividendPayment * cumulativeStocksWithDividend,
+                    withoutDividendProperty: cumulativeStocksWithoutDividend * averageQuote,
+                    withoutDividendContribution: contributionWithoutDividend,
+                    withoutDividendOrdenedStocks: integerPartWithoutDividend,
+                    withoutDividendTotalStocks: cumulativeStocksWithoutDividend,
+                    withoutDividendPayment: dividendPayment * cumulativeStocksWithoutDividend,
                 };
             });
+
+            const getYear = (date: string) => date.split('/')[2];
+
+            const yearlyDividendPayments = quotes.reduce((acc: any, quote: any) => {
+                const year = getYear(quote.date);
+
+                if (!acc[year]) {
+                    acc[year] = {
+                        withDividendPayment: 0,
+                        withoutDividendPayment: 0,
+                    };
+                }
+
+                acc[year].withDividendPayment += quote.withDividendPayment;
+                acc[year].withoutDividendPayment += quote.withoutDividendPayment;
+
+                return acc;
+            }, {});
+
+            const totalWithDividendPayment = quotes.reduce(
+                (sum: number, quote: any) => sum + quote.withDividendPayment,
+                0
+            );
+
+            const totalWithoutDividendPayment = quotes.reduce(
+                (sum: number, quote: any) => sum + quote.withoutDividendPayment,
+                0
+            );
 
             return res.json({ 
                 quotes: quotes,
                 dividends: dividends,
+                results: {
+                    totalPayment: {
+                        withDividendPayment: totalWithDividendPayment,
+                        withoutDividendPayment: totalWithoutDividendPayment,
+                    },
+                    byYear: {
+                        ...yearlyDividendPayments,
+                    }
+                }
             });
         } catch (error) {
             res.status(500).json({ error: 'Erro ao buscar dados da ação.' });
