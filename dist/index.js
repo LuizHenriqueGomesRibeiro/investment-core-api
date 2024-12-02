5df04ba6ab31bdc9d8c405a8770ef30f0dfa43e2
Luiz Gomes <louisnuuk@hotmail.com>
2024-12-02 11:34:56 -0300

    feat

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const index_1 = require("./controller/index");
 const app = (0, express_1.default)();
 const stock = new index_1.Stock();
+app.use((0, cors_1.default)());
 app.get('/stock/list/', stock.getStockValuesList);
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
