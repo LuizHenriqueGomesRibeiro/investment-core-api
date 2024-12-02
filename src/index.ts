e28339378f677505fa71e7f5b62f1e97f797d8f2
Luiz Gomes <louisnuuk@hotmail.com>
2024-12-02 11:23:36 -0300

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { Stock } from './controller/index';
 
 const app = express();
 
 const stock: Stock = new Stock();
 
+app.use(cors());
 app.get('/stock/list/', stock.getStockValuesList);
 
 app.listen(3000, () => {
