bf71adeece77fe57a9bb074e44c771e216cb7c6a
Luiz Gomes <louisnuuk@hotmail.com>
2024-12-06 14:40:17 -0300

    feat

--- src/controller/stock/index.ts
+++ src/controller/stock/index.ts
@@ -28,68 +28,73 @@ export default class Stock {
 
     getStockValuesList = async (req: Request, res: Response) => {
         const { symbol, start, end, reinvestDividend, monthyContribution } = req.query as unknown as GetStockValuesListQuery;
-        const monthyContributionNumbered = Number(monthyContribution);
-
+        const monthlyContributionNumbered = Number(monthyContribution);
+    
         try {
             const stockData: any = await yahooFinance.chart(symbol + '.SA', {
                 period1: new Date(start).getTime() / 1000,
                 period2: new Date(end).getTime() / 1000,
                 interval: '1mo',
             });
-
+    
             const dividends = stockData.events.dividends.map((dividend: any) => ({
                 amount: dividend.amount,
                 date: formatDate(dividend.date),
             }));
-            
+    
             let carryOver = 0;
             let cumulativeStocksWithDividend = 0;
             let cumulativeStocksWithoutDividend = 0;
-            let adjustedContribution = monthyContributionNumbered;
-            let cumulativePayment = 0; 
+            let cumulativePayment = 0;
             let cumulativeContribution = 0;
-
+    
+            const startYear = new Date(start).getFullYear();
+            let adjustedContribution = monthlyContributionNumbered; // Contribuição inicial
+    
             const quotes = stockData.quotes.map((quote: any) => {
-                let date = formatDate(quote.date);
-
-                let dividendPayment = dividends.reduce((sum: any, dividend: any) => {
+                const date = formatDate(quote.date);
+                const quoteYear = new Date(quote.date).getFullYear();
+    
+                // Atualiza a contribuição para 10% a mais a cada ano
+                adjustedContribution = monthlyContributionNumbered * Math.pow(1.1, quoteYear - startYear);
+    
+                const dividendPayment = dividends.reduce((sum: any, dividend: any) => {
                     const [dividendDay, dividendMonth, dividendYear] = dividend.date.split('/').map(Number);
                     const [day, month, year] = date.split('/').map(Number);
-
+    
                     if (dividendMonth === month && dividendYear === year) {
                         return sum + dividend.amount;
                     }
-
+    
                     return sum;
                 }, 0);
-
+    
                 const averageQuote = (quote.open + quote.close) / 2;
-
-                const rawStocksWithoutDividend = monthyContributionNumbered / averageQuote + carryOver;
+    
+                const rawStocksWithoutDividend = monthlyContributionNumbered / averageQuote + carryOver;
                 const integerPartWithoutDividend = Math.floor(rawStocksWithoutDividend);
-
+    
                 const rawStocksWithDividend = adjustedContribution / averageQuote + carryOver;
                 const integerPartWithDividend = Math.floor(rawStocksWithDividend);
-
+    
                 carryOver = rawStocksWithDividend - integerPartWithDividend;
                 cumulativeStocksWithoutDividend += integerPartWithoutDividend;
                 cumulativeStocksWithDividend += integerPartWithDividend;
-
+    
                 const contributionWithDividend = adjustedContribution;
-                const contributionWithoutDividend = monthyContributionNumbered;
-                adjustedContribution = monthyContributionNumbered + dividendPayment * cumulativeStocksWithDividend;
-
+                const contributionWithoutDividend = monthlyContributionNumbered;
+    
                 const currentPayment = dividendPayment * (reinvestDividend ? cumulativeStocksWithDividend : cumulativeStocksWithoutDividend);
                 cumulativePayment += currentPayment;
                 cumulativeContribution += contributionWithoutDividend;
-
+    
                 return reinvestDividend === 'true' ? {
                     quote: averageQuote,
                     date: date,
                     property: cumulativeStocksWithDividend * averageQuote,
                     contribution: contributionWithDividend,
-                    comulativeContribution: cumulativeContribution,
-                    ordenedStocks: integerPartWithDividend,
+                    cumulativeContribution: cumulativeContribution,
+                    orderedStocks: integerPartWithDividend,
                     totalStocks: cumulativeStocksWithDividend,
                     payment: dividendPayment * cumulativeStocksWithDividend,
                     cumulativePayment: cumulativePayment,
@@ -98,54 +103,54 @@ export default class Stock {
                     date: date,
                     property: cumulativeStocksWithoutDividend * averageQuote,
                     contribution: contributionWithoutDividend,
-                    comulativeContribution: cumulativeContribution,
-                    ordenedStocks: integerPartWithoutDividend,
+                    cumulativeContribution: cumulativeContribution,
+                    orderedStocks: integerPartWithoutDividend,
                     totalStocks: cumulativeStocksWithoutDividend,
                     payment: dividendPayment * cumulativeStocksWithoutDividend,
                     cumulativePayment: cumulativePayment,
-                }
+                };
             });
-
+    
             const getYear = (date: string) => date.split('/')[2];
-
+    
             const yearlyDividendPayments = quotes.reduce((acc: any, quote: any) => {
                 const year = getYear(quote.date);
-
+    
                 if (!acc[year]) {
                     acc[year] = {
                         payment: 0,
                     };
                 }
-
+    
                 acc[year].payment += quote.payment;
-
+    
                 return acc;
             }, {});
-
+    
             const transformYearlyDividendPayments = (yearlyDividendPayments: { [key: string]: { payment: number } }): { year: string, payment: number }[] => {
                 return Object.keys(yearlyDividendPayments).map(year => ({
                     year: year,
-                    payment: yearlyDividendPayments[year].payment
+                    payment: yearlyDividendPayments[year].payment,
                 }));
-            }
-
+            };
+    
             const totalPayment = quotes.reduce(
                 (sum: number, quote: any) => sum + quote.payment,
                 0
             );
-
-            return res.json({ 
+    
+            return res.json({
                 quotes: quotes,
                 dividends: dividends,
                 results: {
                     totalPayment: {
                         payment: totalPayment,
                     },
-                    byYear: transformYearlyDividendPayments(yearlyDividendPayments)
-                }
+                    byYear: transformYearlyDividendPayments(yearlyDividendPayments),
+                },
             });
         } catch (error) {
             res.status(500).json({ error: 'Erro ao buscar dados da ação.' });
         }
-    }
+    };
 }
